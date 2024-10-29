926e614585555727cacefad0b89766bc35643461
Matheuzinh0 <matheus.henriquesousa@ufpe.br>
2024-10-29 17:42:49 -0300

    update

--- TESTU01/teste.c
+++ TESTU01/teste.c
@@ -10,9 +10,11 @@ static int s = 32;
 
 int main(){
 unif01_Gen * gen;
-gen= ufile_CreateReadBin ("/home/matheus/Área de Trabalho/hénon_map/arnold.bin",32);
+gen= ufile_CreateReadBin ("TESTU01/output.bin",32);
 void ufile_InitReadBin ();
-bbattery_BlockAlphabit(gen,nb,r,s); // bbattery_RabbitFile("/home/matheus/Área de Trabalho/Resultados z32/prngz32l4_Alphabit.bin",nb);
+bbattery_Rabbit(gen,nb);
+bbattery_Alphabit(gen,nb,r,s);
+
 
 ufile_DeleteReadBin (gen);
   
